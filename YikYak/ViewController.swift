376921ab8297aaf0c6b8d2656e441bdbcc9d0dc2
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-09-27 18:13:40 -0600

    Initial Commit

---
+++ YikYak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YikYak
+//
+//  Created by Ivan Ramirez on 9/27/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
